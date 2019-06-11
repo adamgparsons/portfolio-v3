0c55034f7e177053fc032a7ee2b84ace9a259ff6
Adam <adam@Adams-MacBook-Air.local>
2019-06-11 15:38:13 +0100

    change case

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Hero from "../components/Hero"
 import CaseStudyCollection from "../components/CaseStudyCollection"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
