0a0c507747800496c2cb7206bb3ee72f29cb4b87
Adam <adam@Adams-MacBook-Air.local>
2019-06-11 15:24:55 +0100

    updated case name with js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import Hero from "../components/Hero"
 import CaseStudyCollection from "../components/CaseStudyCollection"
 import Layout from "../components/Layout"
